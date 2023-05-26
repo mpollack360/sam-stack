7e11ec9104e14f714094b47addfe3e5823a330a7
Mark Pollack <mpollack@unicon.net>
2023-05-26 14:18:10 -0500

    add delete

---
+++ src/handlers/delete-by-id.mjs
@@ -0,0 +1,35 @@
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient, DeleteCommand } from '@aws-sdk/lib-dynamodb';
+const client = new DynamoDBClient({});
+const ddbDocClient = DynamoDBDocumentClient.from(client);
+
+const tableName = process.env.CRUD_TABLE;
+
+export const deleteByIdHandler = async (event) => {
+  if (event.httpMethod !== 'DELETE') {
+    throw new Error(`deleteMethod only accept DELETE method, you tried: ${event.httpMethod}`);
+  }
+  console.info('received:', event);
+ 
+  const id = event.pathParameters.id;
+ 
+  var params = {
+    TableName : tableName,
+    Key: { id: id },
+  };
+
+  try {
+    const data = await ddbDocClient.send(new DeleteCommand(params));
+    var item = data.Item;
+  } catch (err) {
+    console.log("Error", err);
+  }
+ 
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(item)
+  };
+ 
+  console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
+  return response;
+}
